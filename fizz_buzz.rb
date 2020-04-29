c675e6250fe4dc7a36a1475c6176908f9a96336c
brian <riceb53@gmail.com>
2020-04-28 21:38:09 -0400

    finishfizz buzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -25,7 +25,14 @@ require 'rspec'
 
 class FizzBuzz
   def output(number)
-    1
+    if number % 3 == 0 && number % 5 == 0
+      return 'fizz buzz'
+    elsif number % 3 == 0
+      return 'fizz'
+    elsif number % 5 == 0
+      return 'buzz'
+    end
+    number
   end
 end
 
@@ -37,5 +44,29 @@ RSpec.describe FizzBuzz do
       fizz_buzz = FizzBuzz.new
       expect(fizz_buzz.output(1)).to eq(1)
     end
+    it 'should return 2 when given 2' do
+      fizz_buzz = FizzBuzz.new
+      expect(fizz_buzz.output(2)).to eq(2)
+    end
+    it 'should return \'fizz\' when given 3' do
+      fizz_buzz = FizzBuzz.new
+      expect(fizz_buzz.output(3)).to eq('fizz')
+    end
+    it 'should return \'buzz\' when given 5' do
+      fizz_buzz = FizzBuzz.new
+      expect(fizz_buzz.output(5)).to eq('buzz')
+    end
+    it 'should return \'fizz\' when given 6' do
+      fizz_buzz = FizzBuzz.new
+      expect(fizz_buzz.output(6)).to eq('fizz')
+    end
+    it 'should return \'buzz\' when given 10' do
+      fizz_buzz = FizzBuzz.new
+      expect(fizz_buzz.output(10)).to eq('buzz')
+    end    
+    it 'should return \'fizz buzz\' when given 15' do
+      fizz_buzz = FizzBuzz.new
+      expect(fizz_buzz.output(15)).to eq('fizz buzz')
+    end    
   end
 end
